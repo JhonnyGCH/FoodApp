25eeea506d15e3b3cae327252b31a38401e8e5dc
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2022-12-23 14:57:11 -0500

    correccion missing constraint layout

--- app/src/main/java/com/example/footapp/infoProfileActivity.kt
+++ app/src/main/java/com/example/footapp/infoProfileActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class infoProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()!!.hide();
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_profile2)
     }
