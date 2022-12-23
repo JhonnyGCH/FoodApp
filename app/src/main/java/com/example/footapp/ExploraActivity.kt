c634702bae089acde11fd42ef55f1c9ba88fceab
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2022-12-23 12:36:39 -0500

    profile en vista explora

--- app/src/main/java/com/example/footapp/ExploraActivity.kt
+++ app/src/main/java/com/example/footapp/ExploraActivity.kt
@@ -1,12 +1,22 @@
 package com.example.footapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ExploraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()!!.hide(); // hide the title bar
         setContentView(R.layout.activity_explora)
+        val profile = findViewById<ImageView>(R.id.user_img)
+        profile.setOnClickListener(){
+            irProfile()
+        }
+    }
+    fun irProfile() {
+        val i = Intent(this, infoProfileActivity::class.java)
+        startActivity(i)
     }
 }
