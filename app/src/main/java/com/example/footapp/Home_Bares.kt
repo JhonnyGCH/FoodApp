2e461449712a934c1a302b04466198eb7c64550b
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2022-12-23 14:03:15 -0500

    se conecto home bares y home rest con la info

--- app/src/main/java/com/example/footapp/Home_Bares.kt
+++ app/src/main/java/com/example/footapp/Home_Bares.kt
@@ -1,12 +1,32 @@
 package com.example.footapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class Home_Bares : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()!!.hide();
         setContentView(R.layout.activity_home_bares)
+        val bar = findViewById<ConstraintLayout>(R.id.constraintLayout2)
+        val bar2 = findViewById<ConstraintLayout>(R.id.constraintLayout)
+        bar2.setOnClickListener(){
+            irBar2()
+        }
+        bar.setOnClickListener(){
+            irBar()
+        }
     }
+    fun irBar2() {
+        val i = Intent(this, infoBaresActivity::class.java)
+        startActivity(i)
+    }
+    fun irBar() {
+        val i = Intent(this, infoBaresActivity::class.java)
+        startActivity(i)
+    }
+
 }
--- app/src/main/java/com/example/footapp/Home_Restaurant.kt
+++ app/src/main/java/com/example/footapp/Home_Restaurant.kt
@@ -1,12 +1,31 @@
 package com.example.footapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class Home_Restaurant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()!!.hide();
         setContentView(R.layout.activity_home_restaurant)
+        val rest = findViewById<ConstraintLayout>(R.id.constraintLayout2)
+        val rest2 = findViewById<ConstraintLayout>(R.id.constraintLayout)
+        rest2.setOnClickListener(){
+            irBar2()
+        }
+        rest.setOnClickListener(){
+            irBar()
+        }
     }
+    fun irBar2() {
+        val i = Intent(this, infoRestaurante::class.java)
+        startActivity(i)
+    }
+    fun irBar() {
+        val i = Intent(this, infoRestaurante::class.java)
+        startActivity(i)
+    }
+
 }
