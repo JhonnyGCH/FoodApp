f9f7ab5d667b7fd6ca197e4d97aed4a98e4ec571
Génesis Daniela Vargas Jáuregui <genesisdvargas4@gmail.com>
2022-12-23 14:52:21 -0500

    se agregan iconos

--- app/src/main/java/com/example/footapp/infoProfileActivity.kt
+++ app/src/main/java/com/example/footapp/infoProfileActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class infoProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getSupportActionBar()!!.hide(); // hide the title bar
         setContentView(R.layout.activity_info_profile2)
     }
 }
