b848db190379665c482df436f5179f7aaa7e0ecc
Daniela  Juliana Sanchez Bayona <danielajulianasbay@ufps.edu.co>
2022-12-22 23:54:44 -0500

    Mis reservas

---
+++ app/src/main/java/com/example/footapp/MisReservasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.footapp
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MisReservasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_reservas)
+    }
+}
