bf0ca591dce6289b6c1b0ad5c1114234f2753a0c
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2022-12-23 11:14:29 -0500

    Vista explorar

---
+++ app/src/main/java/com/example/footapp/ExploraActivity.kt
@@ -0,0 +1,11 @@
+package com.example.footapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExploraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explora)
+    }
+}
